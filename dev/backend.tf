ceac6999df2e07d70b71cf5dfc5710ba4f5b6338
karin <ovbkarin@gmail.com>
2025-06-03 02:14:08 -0400

    deploying infra-tech 1

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "dev/terraform.state"
+    key            = "prod/terraform.state"
     bucket         = "infra-tech-bucket"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking-project"
